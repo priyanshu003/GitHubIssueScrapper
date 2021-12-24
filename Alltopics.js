76df52c93ac250c19acfd1eb8e6385d213bce6f4
Priyanshu <jainpriyanshu341@gmail.com>
2021-12-24 16:35:26 +0530

    Initial commit

---
+++ Alltopics.js
@@ -0,0 +1,32 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const getIssuesPageHtml = require("./issue");
+
+function getReposPageHtml(url, topic) {
+  request(url, cb);
+  function cb(err, response, html) {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode == 404) {
+      console.log("page not found");
+    } else {
+      getReposLink(html);
+    }
+  }
+  function getReposLink(html) {
+    let $ = cheerio.load(html);
+    let headingsArr = $(".f3.color-fg-muted.text-normal.lh-condensed");
+    console.log(topic);
+    for (let i = 0; i < 8; i++) {
+      let twoAnchors = $(headingsArr[i]).find("a");
+      let link = $(twoAnchors[1]).attr("href");
+      let fullLink = `https://github.com${link}/issues`;
+      let repoName = link.split("/").pop();
+	 // console.log(repoName);
+      getIssuesPageHtml(fullLink, topic, repoName);
+    }
+    console.log("```````````````````````");
+  }
+}
+
+module.exports = getReposPageHtml;
